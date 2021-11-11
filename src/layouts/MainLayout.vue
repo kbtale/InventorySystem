9a3798935e53da183ddfa9e4b467b578bfc04962
Carlos Bolívar <36908755+kbtale@users.noreply.github.com>
2021-11-11 12:50:38 -0400

    Add files via upload

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,151 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header elevated reveal>
+      <q-toolbar class="q-pt-sm q-pb-sm">
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          size="lg"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          CEVAZ
+        </q-toolbar-title>
+        <q-btn-group push>
+        <q-btn
+          push
+          color="secondary"
+          size="md"
+          label="Add new object"
+          icon="add_circle"
+        />
+        <q-btn
+          push
+          color="purple"
+          size="md"
+          label="Update object"
+          icon="mode_edit"
+        />
+        <q-btn
+          push
+          color="negative"
+          size="md"
+          label="Delete object"
+          icon="delete_forever"
+        />
+      </q-btn-group>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Departments
+        </q-item-label>
+
+        <DepartmentsMenu
+          v-for="link in departments"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import DepartmentsMenu from 'components/departmentsMenu.vue'
+
+const departmentsList = [
+  {
+    title: 'Academy',
+    caption: 'Ground Floor',
+    icon: 'school',
+    link: 'Academy'
+  },
+  {
+    title: 'Administration',
+    caption: 'Floor 1',
+    icon: 'manage_accounts',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Library',
+    caption: 'Ground Floor',
+    icon: 'library_books',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Human Resources',
+    caption: 'Ground Floor',
+    icon: 'people',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Customer Support',
+    caption: 'Ground Floor',
+    icon: 'support_agent',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Marketing',
+    caption: 'Floor 1',
+    icon: 'moving',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'IT',
+    caption: 'Ground Floor',
+    icon: 'computer',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Manteinance',
+    caption: 'Ground Floor',
+    icon: 'settings',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'CEVAZ',
+    caption: 'Different Floors',
+    icon: 'domain',
+    link: 'https://awesome.quasar.dev'
+  }
+];
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    DepartmentsMenu
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      departments: departmentsList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ src/pages/crudObject.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-page class="q-pa-md">
+    <div class="row flex-center">
+      <q-card class="q-pa-lg col-9 col-xs-12 col-sm-10 col-md-9 col-lg-8 col-xl-6">
+        <q-form
+          @submit="onSubmit"
+          @reset="onReset"
+          class="q-gutter-md"
+        >
+          <div class="row justify-between">
+          <q-select class="col-4 col-xs-10 col-sm-6 col-md-4 col-lg-4 col-xl-4 q-pa-xs" outlined v-model="selection.objectType" :options="objectsList" label="Object Type" options-dense="true" />
+          <q-select class="col-4 col-xs-10 col-sm-6 col-md-4 col-lg-4 col-xl-4 q-pa-xs" outlined v-model="selection.location" :options="locationsList" label="Location" options-dense="true" />
+          <q-select class="col-4 col-xs-10 col-sm-6 col-md-4 col-lg-4 col-xl-4 q-pa-xs" outlined v-model="selection.status" :options="statusesList" label="Status" options-dense="true" />
+          </div>
+          <div class="row justify-start">
+          <q-input class="col-4 col-xs-10 col-sm-6 col-md-4 col-lg-4 col-xl-4 q-pa-xs" outlined v-model="selection.brand" label="Brand" />
+          <q-input class="col-4 col-xs-10 col-sm-6 col-md-4 col-lg-4 col-xl-4 q-pa-xs" outlined v-model="selection.model" label="Model" />
+          <q-input class="col-4 col-xs-10 col-sm-6 col-md-4 col-lg-4 col-xl-4 q-pa-xs" outlined v-model="selection.serial" label="Serial" />
+          <q-input class="col-4 col-xs-10 col-sm-6 col-md-4 col-lg-4 col-xl-4 q-pa-xs" outlined v-model="selection.color" label="Color" />
+          <q-input class="col-4 col-xs-10 col-sm-6 col-md-4 col-lg-4 col-xl-4 q-pa-xs" outlined autogrow v-model="selection.observations" label="Observations" />
+          </div>
+          <div>
+            <q-btn label="Submit" type="submit" color="primary"/>
+            <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+          </div>
+        </q-form>
+      </q-card>
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'CrudObject',
+  data(){
+    return {
+      objectsList: [
+        "Computer",
+        "Television",
+        "Scanner",
+        "Printer",
+        "Speakers"
+      ],
+      locationsList: [
+        "Academy",
+        "Human Resources",
+        "Accounting",
+        "Administration",
+        "IT"
+      ],
+      statusesList: [
+        "Ideal",
+        "Working",
+        "Being Repaired",
+        "Administration",
+        "IT"
+      ],
+      selection: {
+        objectType: ''
+      }
+    }
+  }
+}
+</script>
